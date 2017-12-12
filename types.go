7816ab5058173eec360911ff241f289f4191848e
carlo.maiorano <pasqualecarlo.maiorano@yahoo.it>
2017-12-12 15:10:30 +0100

    charging

--- types.go
+++ types.go
@@ -9,6 +9,40 @@ const (
 	sandBoxAPIEndpoint = "https://staging.authservices.satispay.com/online"
 )
 
+//ChargeStatus represent the status of requested Charge
+type ChargeStatus int
+
+const (
+	//Required Charge sent to a user waitng for acceptance
+	Required ChargeStatus = iota
+
+	//Success Charge accepted by the user
+	Success
+
+	//Failure Charge failed, more details can be found on ChargeStatusDetail
+	Failure
+)
+
+//ChargeStatusDetail represent the detail regarding a failure of Charge operation
+type ChargeStatusDetail int
+
+const (
+	//DeclinedByPayer user declined the Charge
+	DeclinedByPayer ChargeStatusDetail = iota
+
+	//DeclinedByPayerNotRequired user declined the Charge because he did not request it
+	DeclinedByPayerNotRequired
+
+	//CancelByNewCharge same Charge sent to the same user, the second will override the first
+	CancelByNewCharge
+
+	//InternalFailure generic error
+	InternalFailure
+
+	//Expired the Charge has expired
+	Expired
+)
+
 type Client struct {
 	bearerToken string
 	endpoint    string
